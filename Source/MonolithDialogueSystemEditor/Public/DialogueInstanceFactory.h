570a2816bfb03a028f7470c201ebbebd366fe4ec
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2025-01-09 11:10:15 -0500

    Create DialogueInstanceFactory

---
+++ Source/MonolithDialogueSystemEditor/Public/DialogueInstanceFactory.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Factories/Factory.h"
+#include "DialogueInstanceFactory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MONOLITHDIALOGUESYSTEMEDITOR_API UDialogueInstanceFactory : public UFactory
+{
+	GENERATED_BODY()
+};
