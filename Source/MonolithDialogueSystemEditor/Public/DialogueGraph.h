4274aeccce896832e614f19f6961df3240fadd9f
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2025-01-10 14:03:21 -0500

    Created DialogueGraph

---
+++ Source/MonolithDialogueSystemEditor/Public/DialogueGraph.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EdGraph/EdGraph.h"
+#include "DialogueGraph.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MONOLITHDIALOGUESYSTEMEDITOR_API UDialogueGraph : public UEdGraph
+{
+	GENERATED_BODY()
+};
