e96d712010b4e8013766d459bf9d66c9be91bf74
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2025-01-09 21:01:01 -0500

    Created DialogueGraphNode_Response

---
+++ Source/MonolithDialogueSystemEditor/Public/DialogueGraphNode_Response.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DialogueGraphNode.h"
+#include "DialogueGraphNode_Response.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MONOLITHDIALOGUESYSTEMEDITOR_API UDialogueGraphNode_Response : public UDialogueGraphNode
+{
+	GENERATED_BODY()
+};
