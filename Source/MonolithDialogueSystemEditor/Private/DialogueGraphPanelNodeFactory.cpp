fdd5e2335a4331882bed27e4988c0191a58c3504
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2025-01-18 14:13:09 -0500

    Dialogue Query and Response nodes use SDialogueGraphNode for content area

--- Source/MonolithDialogueSystemEditor/Private/DialogueGraphPanelNodeFactory.cpp
+++ Source/MonolithDialogueSystemEditor/Private/DialogueGraphPanelNodeFactory.cpp
@@ -4,6 +4,8 @@
 #include "DialogueGraphPanelNodeFactory.h"
 
 #include "DialogueGraphNode_Alias.h"
+#include "DialogueGraphNode_Dialogue.h"
+#include "SDialogueGraphNode.h"
 #include "SDialogueGraphNode_Alias.h"
 
 
@@ -12,6 +14,10 @@ TSharedPtr<SGraphNode> FDialogueGraphPanelNodeFactory::CreateNode(UEdGraphNode* 
 	if (UDialogueGraphNode_Alias* AliasNode = Cast<UDialogueGraphNode_Alias>(Node))
 	{
 		return SNew(SDialogueGraphNode_Alias, AliasNode);
+	}
+	if (UDialogueGraphNode_Dialogue* DialogueNode = Cast<UDialogueGraphNode_Dialogue>(Node))
+	{
+		return SNew(SDialogueGraphNode, DialogueNode);
 	}
 	
 	return nullptr;
--- Source/MonolithDialogueSystemEditor/Private/SDialogueGraphNode.cpp
+++ Source/MonolithDialogueSystemEditor/Private/SDialogueGraphNode.cpp
@@ -19,7 +19,7 @@ FText SDialogueGraphNode::GetDialogueText() const
 	// UDialogueNodeData_Alias* NodeData = Cast<UDialogueNodeData_Alias>(AliasNode->GetDialogueNodeData());
 	// return NodeData->AliasName;
 
-	return FText::FromString("");
+	return FText::FromString("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  ");
 }
 
 void SDialogueGraphNode::UpdateGraphNode()
@@ -52,6 +52,7 @@ TSharedRef<SWidget> SDialogueGraphNode::CreateNodeContentArea()
 				// DIALOGUE TEXT
 				SAssignNew(DialogueText, STextBlock)
 				.Text(this, &SDialogueGraphNode::GetDialogueText)
+				.WrapTextAt(250.0f)
 			]
 			+SHorizontalBox::Slot()
 			.AutoWidth()
