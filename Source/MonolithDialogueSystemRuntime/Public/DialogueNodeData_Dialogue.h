2863c395c64ceb109c3a2c0f72010065f42dbaed
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2025-01-18 14:32:49 -0500

    Created DialogueNodeData_Dialogue

---
+++ Source/MonolithDialogueSystemRuntime/Public/DialogueNodeData_Dialogue.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DialogueNodeData_Base.h"
+#include "DialogueNodeData_Dialogue.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MONOLITHDIALOGUESYSTEMRUNTIME_API UDialogueNodeData_Dialogue : public UDialogueNodeData_Base
+{
+	GENERATED_BODY()
+};
